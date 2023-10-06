fa253f0d362ee1c4bd39867797680bfb9749439e
hossamelden202 <138601679+hossamelden202@users.noreply.github.com>
2023-10-06 23:00:20 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 
@@ -16,7 +23,7 @@ int main(void)
 		}
 	}
 
-	putchar(10);
+	putchar(10); /* this is an ascii code for new line*/
 
 	return (0);
 }
