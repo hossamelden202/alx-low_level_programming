ac90bc21becd31d4868462405565cde44dbdf05f
hossam <hossameldenmohamed202@gmail.com>
2023-10-22 15:12:16 +0300

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * main - check the code
- * Return: Always 0
+ * main - the driver function print _putchar followed on a new line
+ * Return: always 0
  */
 int main(void)
 {
-	char x[20] = {"_putchar"};
-
-	putchar('_');
-
-	for (char i = 0; i < 20; i++)
-
-		putchar(x[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-
-
 }
